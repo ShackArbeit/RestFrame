870d3a7ebab47a4c545760162e0d62c4e57bf8cf
ShackLin <g0972222165@gmail.com>
2024-08-11 14:22:17 +0800

    'AddMixinsViews'

--- store/urls.py
+++ store/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     # 這是使用 function view 的樣式
     # path('products/<int:id>/',views.product_detail),
     # 這是使用 class view 的樣式
-    path('products/<int:id>',views.ProductDetail.as_view()),
-    path('products/',views.collection_list),
-    path('collections/<int:pk>/',views.collection_detail,name='collection-detail')
+    path('products/<int:pk>',views.ProductDetail.as_view()),
+    path('collection/',views.CollectionList.as_view()),
+    path('collections/<int:pk>/',views.CollectionDetail.as_view(),name='collection-detail')
 ]
--- store/views.py
+++ store/views.py
@@ -5,43 +5,50 @@ from rest_framework.response import Response
 from rest_framework import status 
 # 使用 class views 
 from rest_framework.views import APIView
+# 使用 Mixins, Generics
+from rest_framework.mixins import ListModelMixin
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Product,Collection
 from .serializers import ProductSerializer,CollectionSerializer
 from django.db.models import Count
 
 # 這裡是使用 class views 的樣式
-class ProductList(APIView):
-    def get(self,request):
-        queryset=Product.objects.all()
-        serializer=ProductSerializer(queryset,many=True,context={
-            'request':request
-        })
-        return Response(serializer.data)
-    def post(self,request):
-        serializer=ProductSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        print(serializer.validated_data)
-        return Response(serializer.data,status=status.HTTP_201_CREATED)
-
-class ProductDetail(APIView):
-    def get(self,request,id):
-        product=get_object_or_404(Product,pk=id)
-        serializer=ProductSerializer(product)
-        return Response(serializer.data)
-    def put(self,request,id):
-        product=get_object_or_404(Product,pk=id)
-        serializer=ProductSerializer(product,data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    def delete(self,request,id):
-        product=get_object_or_404(Product,pk=id)
+class ProductList(ListCreateAPIView):
+    queryset=Product.objects.select_related('collection').all()
+    serializer_class=ProductSerializer
+    def get_serializer_context(self):
+        return {'request':self.request}
+    
+    
+class ProductDetail(RetrieveUpdateDestroyAPIView):
+    queryset=Product.objects.all()
+    serializer_class=ProductSerializer
+    def delete(self,request,pk):
+        product=get_object_or_404(Product,pk=pk)
         if product.orderitems.count()>0:
             return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+# 這裡是使用 class views 的樣式
+class CollectionList(ListCreateAPIView):
+    queryset=Collection.objects.annotate(
+        products_count=Count('products').all()
+    )
+    serializer_class=CollectionSerializer
+
+
+class CollectionDetail(RetrieveUpdateDestroyAPIView):
+    queryset=Collection.objects.annotate(
+        products_count=Count('products')
+    )
+    serializer_class=CollectionSerializer
+    def delete(self,request,pk):
+        collection=get_object_or_404(Collection,pk=pk)
+        if collection.products.count()>0:
+            return Response({'error':'Could not be deleted !'})
+        collection.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view(['GET','POST'])
@@ -88,7 +95,7 @@ def product_detail(request,id):
 @api_view(['GET','POST'])
 def collection_list(request):
     if request.method=='GET':
-        queryset=Collection.objects.annotate(products_count=Count('products')).a
+        queryset=Collection.objects.annotate(products_count=Count('products')).all()
         serializer=CollectionSerializer(queryset,many=True)
         return Response(serializer.data)
     elif request.method=='POST':

dd0312c466b8755822e9e5eb0867e1d909c242f6
Shack <g0972222165@gmail.com>
2024-08-14 10:39:35 +0800

    Add Filter

--- store/views.py
+++ store/views.py
@@ -13,11 +13,25 @@ from .serializers import ProductSerializer,CollectionSerializer,ReviewSerializer
 from django.db.models import Count
 # 使用 Views Set
 from rest_framework.viewsets import ModelViewSet
+# 使用 Django Filter
+from django_filters.rest_framework import DjangoFilterBackend
+from .filters import ProductFilter
 
 
 class ProductViewSet(ModelViewSet):
     queryset=Product.objects.all()
     serializer_class=ProductSerializer
+    filter_backends=[DjangoFilterBackend]
+    # filterset_fields=['collection_id']
+    filterset_class=ProductFilter
+   # 因為有使用 Django-Filter ，所以可以簡化不用以下
+    # def get_queryset(self):
+    #     queryset=Product.objects.all()
+    #     collection_id=self.request.query_params.get('collection_id')
+    #     if collection_id is not None:
+    #         queryset=queryset.filter(collection_id=collection_id)
+    #     return queryset
+
     def get_serializer_context(self):
         return {'request':self.request}
        
